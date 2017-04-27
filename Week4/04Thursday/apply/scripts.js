d58a035cf32cb6d3b00d4cf99bdd8d9a0ad8fae1
Lucas Dina <lukeadina@gmail.com>
2017-04-27 17:32:37 -0600

    All school stuff so far
    just a bunch of html and css3 stuff

---
+++ Week4/04Thursday/apply/scripts.js
@@ -0,0 +1,1 @@
+setTimeout (function message() { alert("Use JavaScript to create pop-up messages to capture the user’s attention.") }, 3000);
---
+++ Week4/04Thursday/fitness/scripts/myFunctions.js
@@ -0,0 +1,25 @@
+/*Lucas Dina*/
+//Thank you alert
+/*jslint devel: true */
+/*ignore jslint start*/
+
+function tyAlert() {
+    'use strict';
+    alert("Thank you for your interest in our fitness club. We will contact you soon.");
+}
+
+function burpees() {
+    'use strict';
+    window.open("scripts/burpees.html", "_blank", "width=610, height=360");
+}
+
+function plank() {
+    'use strict';
+    window.open("scripts/plank.html",  "_blank", "width=610, height=360");
+}
+
+function mtnClimber() {
+    'use strict';
+    window.open("scripts/mtn.html",  "_blank", "width=610, height=360");
+}
+/*jshint ignore: end*/
